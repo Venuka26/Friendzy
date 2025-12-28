03d0b853f7611a09b6712b0a04fa814bfca33a7e
Venuka26 <dewanganvenu20@gmail.com>
2025-12-28 22:35:48 +0530

    add user profile fetch and update with image upload support

---
+++ server/controllers/userController.js
@@ -0,0 +1,98 @@
+import imagekit from "../configs/imageKit.js"
+import User from "../models/User.js"
+import fs from 'fs'
+import { clerkClient } from "@clerk/express";
+
+
+// Get User Data using userId
+export const getUserData = async (req, res) => {
+    try {
+        const { userId } = req.auth()
+        const user = await User.findById(userId)
+        if(!user){
+            return res.json({success: false, message: "User not found"})
+        }
+        res.json({success: true, user})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+//  Update User Data
+export const updateUserData = async (req, res) => {
+    try {
+        const { userId } = req.auth()
+        let {username, bio, location, full_name } = req.body;
+
+        const tempUser = await User.findById(userId)
+
+        !username && (username = tempUser.username)
+
+        if(tempUser.username !== username){
+            const user = await User.findOne({username})
+            if(user){
+                // we will not change the username if it is already taken
+                username = tempUser.username
+            }
+        }
+
+        const updatedData = {
+            username,
+            bio,
+            location,
+            full_name
+        }
+
+        const profile = req.files.profile && req.files.profile[0]
+        const cover = req.files.cover && req.files.cover[0]
+
+        if(profile){
+            const buffer = fs.readFileSync(profile.path)
+            const response = await imagekit.upload({
+                file: buffer,
+                fileName: profile.originalname,
+            })
+
+            const url = imagekit.url({
+                path: response.filePath,
+                transformation: [
+                    {quality: 'auto'},
+                    { format: 'webp' },
+                    { width: '512' }
+                ]
+            })
+            updatedData.profile_picture = url;
+
+            const blob = await fetch(url).then(res => res.blob());
+            await clerkClient.users.updateUserProfileImage(userId, { file: blob });
+        }
+
+        if(cover){
+            const buffer = fs.readFileSync(cover.path)
+            const response = await imagekit.upload({
+                file: buffer,
+                fileName: profile.originalname,
+            })
+
+            const url = imagekit.url({
+                path: response.filePath,
+                transformation: [
+                    {quality: 'auto'},
+                    { format: 'webp' },
+                    { width: '1280' }
+                ]
+            })
+            updatedData.cover_photo = url;
+        }
+
+        const user = await User.findByIdAndUpdate(userId, updatedData, {new : true})
+
+        res.json({success: true, user, message: 'Profile updated successfully'})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
