2105328b2e98a8198cc7101afa73e01a61b390d1
Venuka26 <dewanganvenu20@gmail.com>
2025-12-29 11:32:59 +0530

    add user discovery and follow/unfollow functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -96,3 +96,79 @@ export const updateUserData = async (req, res) => {
     }
 }
 
+// Find Users using username, email, location, name
+
+export const discoverUsers = async (req, res) => {
+    try {
+        const { userId } = req.auth()
+        const { input } = req.body;
+
+        const allUsers = await User.find(
+            {
+                $or: [
+                    {username: new RegExp(input, 'i')},
+                    {email: new RegExp(input, 'i')},
+                    {full_name: new RegExp(input, 'i')},
+                    {location: new RegExp(input, 'i')},
+                ]
+            }
+        )
+        const filteredUsers = allUsers.filter(user=> user._id !== userId);
+
+        res.json({success: true, users: filteredUsers})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Follow User
+
+export const followUser = async (req, res) => {
+    try {
+        const { userId } = req.auth()
+        const { id } = req.body;
+
+        const user = await User.findById(userId)
+
+        if(user.following.includes(id)){
+            return res.json({ success: false, message: 'You are already following this user'})
+        }
+
+        user.following.push(id);
+        await user.save()
+
+        const toUser = await User.findById(id)
+        toUser.followers.push(userId)
+        await toUser.save()
+
+        res.json({success: true, message: 'Now you are following this user'})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Unfollow User
+export const unfollowUser = async (req, res) => {
+    try {
+        const { userId } = req.auth()
+        const { id } = req.body;
+
+        const user = await User.findById(userId)
+        user.following = user.following.filter(user=> user !== id);
+        await user.save()
+
+        const toUser = await User.findById(id)
+        toUser.followers = toUser.followers.filter(user=> user !== userId);
+        await toUser.save()
+        
+        res.json({success: true, message: 'You are no longer following this user'})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
